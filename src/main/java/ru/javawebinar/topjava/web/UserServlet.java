f9c2dea5340a9f2c9764769c79b228108483165e
SeleznevAM <seleznevam84@ya.ru>
2016-06-07 19:24:04 +0300

    Start of HW01

--- src/main/java/ru/javawebinar/topjava/web/UserServlet.java
+++ src/main/java/ru/javawebinar/topjava/web/UserServlet.java
@@ -16,12 +16,13 @@ import static org.slf4j.LoggerFactory.getLogger;
  * Date: 19.08.2014
  */
 public class UserServlet extends HttpServlet {
+
     private static final Logger LOG = getLogger(UserServlet.class);
 
     protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
         LOG.debug("redirect to userList");
 
-//        request.getRequestDispatcher("/userList.jsp").forward(request, response);
+//        request.getRequestDispatcher("/userList.jsp").forward(request, response); строка для перенаправления
         response.sendRedirect("userList.jsp");
     }
 }
